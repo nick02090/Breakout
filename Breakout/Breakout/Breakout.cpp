e2b3df25421b6686eccf18937ac13b8e9b184c95
Nikola Ćališ <nidjolas.u.b@gmail.com>
2020-09-03 21:05:05 +0200

    Install required libraries

---
+++ Breakout/Breakout/Breakout.cpp
@@ -0,0 +1,14 @@
+// Breakout.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+int main(int argc, char* args[])
+{
+    std::cout << "Starting your game..." << std::endl;
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
